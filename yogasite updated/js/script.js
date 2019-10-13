73fd7ee37357c4a40396ebd5323a95be2357af7e
exampleLanding <53074245+exampleLanding@users.noreply.github.com>
2019-10-13 20:54:02 +0700

    Update script.js
    added all scripts

--- yogasite updated/js/script.js
+++ yogasite updated/js/script.js
@@ -117,4 +117,160 @@ window.addEventListener('DOMContentLoaded', function() {
             });
         };
     });
+    
+    // Calc
+    
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener('change', function() {
+            personsSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+
+            if (restDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else if (persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        restDays.addEventListener('change', function() {
+            daysSum = +this.value;
+            total = (daysSum + personsSum)*4000;
+
+            if (persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else if (restDays.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total;
+            }
+        });
+
+        place.addEventListener('change', function() {
+            if (restDays.value == '' || persons.value == '') {
+                totalValue.innerHTML = 0;
+            } else {
+                let a = total;
+                totalValue.innerHTML = a * this.options[this.selectedIndex].value; // определить value у option
+            }
+        });
+    
+    //Form
+    
+    let message = {
+        loading: 'Загрузка...',
+        success: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так :('
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+        statusMessage.classList.add('status');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        form.appendChild(statusMessage);
+
+        let request = new XMLHttpRequest();
+        request.open('POST', 'server.php');
+        // request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); - заголовок для формы
+
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        // атрибут name создает ключ в объекте когда данные пережаются на сервер а значение передает сам пользователь
+
+        let formData = new FormData(form); // помещаем в данный объект ту форму которую заполнил пользователь
+        
+        let obj = {};
+        formData.forEach(function(value, key) {
+            obj[key] = value; // берем объект formData и все данные которые есть в нем помещаем в объект obj
+        });
+        let json = JSON.stringify(obj);
+
+        request.send(json); // передаем данные которые ввел пользовательв форме (body)
+        // request.send(formData); передаем данные не в формате json
+        
+        request.addEventListener('readystatechange', function() {
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.success; // message.failure по факту но я не знаю как пофиксить ошибку 405
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
+    
+    //Slider
+    
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            slideIndex = 1; // перемещаемся на первый если в конце нажимаем вперед
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length // перемещаемся на последний слайд если в начале нажимаем назад
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        // for (let i = 0; i < slides.length; i++) { одно и тоже
+        // slides[i].style.display = 'none';
+        // }
+
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n); 
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1); // уменьшаем слайд индекс на 1 , идем влево
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1); // увеличиваем слайд индекс на 1 , идем вправо
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i);
+            }
+        }
+    });
+    
+    
 });
